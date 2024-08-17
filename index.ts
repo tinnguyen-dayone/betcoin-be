cd05b8c0482c37dfad01e6924c2a842e99405309
tinnguyen-dayone <tinnguyen@dayoneteams.com>
2024-08-17 23:44:20 +0700

    fix swagger

--- index.ts
+++ index.ts
@@ -13,28 +13,31 @@ app.use(express.json())
 const port = process.env.PORT || 8000;
 
 app.post('/auth/google', async (req, res) => {
-    const { idToken } = req.body;
+    const {idToken} = req.body;
     try {
         const userPayload = await verifyToken(idToken);
-        res.status(200).json({ success: true, user: userPayload });
+        res.status(200).json({success: true, user: userPayload});
     } catch (error) {
-        res.status(400).json({ success: false, message: 'Invalid ID token' });
+        res.status(400).json({success: false, message: 'Invalid ID token'});
     }
 });
 
 
 app.post('/auth/facebook', async (req, res) => {
-    const { accesstoken } = req.body;
+    const {accesstoken} = req.body;
     try {
         const userPayload = await verifyFacebookToken(accesstoken);
-        res.status(200).json({ success: true, userPayload });
-    }catch (error) {
-        res.status(400).json({ success: false, message: 'Invalid Facebook token' });
+        res.status(200).json({success: true, userPayload});
+    } catch (error) {
+        res.status(400).json({success: false, message: 'Invalid Facebook token'});
     }
 
 })
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOutput));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOutput, {
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    customJs: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js'
+}));
 
 app.get('/', (req, res) => {
     res.redirect('/api-docs');
